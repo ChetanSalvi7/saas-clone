1e8966f014fb59bfd2bfc81da2a549238c185caa
Chetan Salvi <salvi5375@gmail.com>
2022-08-05 12:03:22 +0530

    background animation add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import  Header  from './Components/Header/Header';
+import Header from './Components/Header/Header';
 import './App.css';
 import Header_menu from './Components/Header/Header_menu';
 import Home from './Components/Home/Home';
+import BAnimation from './Components/Background Animation/BAnimation';
 
 function App() {
   return (
     <>
-    <Header />
-    <Header_menu />
-    <Home />
-    
+
+      <Header />
+      <Header_menu />
+      <Home />
+      <BAnimation />
     </>
   );
 }
---
+++ src/Components/Background Animation/BAnimation.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const BAnimation = () => {
+  return (
+   <>
+    <div class="area" >
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div >
+   </>
+  )
+}
+
+export default BAnimation
